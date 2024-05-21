0839c891ea0c3e52b492c62cd0a4fe82a039f3c0
Yulian Yordanov <yul.yordanov@gmail.com>
2024-05-21 20:21:23 +0300

    Initial commit

---
+++ Programming Fundamentals and Unit Testing - MAY 2024/05 Simple Commands and Visual Studio - Exercise/InchesToCentimeters/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace InchesToCentimeters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inches = int.Parse(Console.ReadLine());
+            Console.WriteLine(inches * 2.54);
+        }
+    }
+}
